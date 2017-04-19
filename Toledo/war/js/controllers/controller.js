fa1f360fae52d5ce2c0ec5a8cd8212dab3a04cfe
memogomez <Memo@LAPTOP-8BBOI0AH>
2017-04-19 10:35:40 -0500

    Cambios Conf

---
+++ Toledo/war/js/controllers/controller.js
@@ -0,0 +1,9 @@
+var app = angular.module("app", [ 'ngRoute', 'ngCookies' ]);
+app.config([ '$routeProvider', function($routeProvider) {
+
+//	$routeProvider.when('/empresas', {
+//		templateUrl : "pages/empresas.html",
+//		controller : "empresasController"
+//	});
+	
+} ]);
