b5172780d73f6134f4bc2b895ab1e7b426ad3658
memogomez <Memo@LAPTOP-8BBOI0AH>
2017-04-19 14:02:52 -0500

    Cambios html princ

--- Toledo/war/js/controllers/controller.js
+++ Toledo/war/js/controllers/controller.js
@@ -1,10 +1,25 @@
 var app = angular.module("app", [ 'ngRoute', 'ngCookies' ]);
 app.config([ '$routeProvider', function($routeProvider) {
 
-//	$routeProvider.when('/empresas', {
-//		templateUrl : "pages/empresas.html",
-//		controller : "empresasController"
-//	});
-	
+	$routeProvider.when('/inicio', {
+		templateUrl : "pages/inicio.html",
+//		controller : "controller"
+	});
+	$routeProvider.when('/clientes', {
+		templateUrl : "pages/clientes.html",
+//		controller : "controller"
+	});
+	$routeProvider.when('/proveedores', {
+		templateUrl : "pages/proveedores.html",
+//		controller : "controller"
+	});
+	$routeProvider.when('/herramientas', {
+		templateUrl : "pages/herramientas.html",
+//		controller : "controller"
+	});
+	$routeProvider.when('/tornillos', {
+		templateUrl : "pages/tornillos.html",
+//		controller : "controller"
+	});
 	
 } ]);
