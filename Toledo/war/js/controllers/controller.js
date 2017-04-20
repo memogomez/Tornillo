8da0069ebbc8d1fab38fe2021b277d9f9a78fc99
memogomez <Memo@LAPTOP-8BBOI0AH>
2017-04-20 10:52:46 -0500

    Registro de Clientes Proveedores Herramientas

--- Toledo/war/js/controllers/controller.js
+++ Toledo/war/js/controllers/controller.js
@@ -7,35 +7,35 @@ app.config([ '$routeProvider', function($routeProvider) {
 	});
 	$routeProvider.when('/clientes', {
 		templateUrl : "pages/clientes.html",
-//		controller : "controller"
+		controller : "clientesController"
 	});
 	$routeProvider.when('/proveedores', {
 		templateUrl : "pages/proveedores.html",
-//		controller : "controller"
+		controller : "proveedoresController"
 	});
 	$routeProvider.when('/herramientas', {
 		templateUrl : "pages/herramientas.html",
-//		controller : "controller"
+		controller : "herramientasController"
 	});
 	$routeProvider.when('/tornillos', {
 		templateUrl : "pages/tornillos.html",
-//		controller : "controller"
+		controller : "tornillosController"
 	});
 	$routeProvider.when('/altaCliente', {
 		templateUrl : "pages/altaCliente.html",
-//		controller : "controller"
+		controller : "clientesController"
 	});
 	$routeProvider.when('/altaProveedor', {
 		templateUrl : "pages/altaProveedor.html",
-//		controller : "controller"
+		controller : "proveedoresController"
 	});
 	$routeProvider.when('/altaHerramienta', {
 		templateUrl : "pages/altaHerramienta.html",
-//		controller : "controller"
+		controller : "herramientasController"
 	});
 	$routeProvider.when('/altaTornillos', {
 		templateUrl : "pages/altaTornillos.html",
-//		controller : "controller"
+		controller : "tornillosController"
 	});
 	$routeProvider.when('/inventario', {
 		templateUrl : "pages/inventario.html",
