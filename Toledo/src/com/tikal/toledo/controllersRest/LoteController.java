a6f5f741af2664682fe8d851d177cbe590394832
Israel <Israel@192.168.1.68>
2017-04-27 15:59:18 -0500

    loteses

--- Toledo/src/com/tikal/toledo/controllersRest/LoteController.java
+++ Toledo/src/com/tikal/toledo/controllersRest/LoteController.java
@@ -1,6 +1,7 @@
 package com.tikal.toledo.controllersRest;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.tikal.toledo.controllersRest.VO.ListaLotesVO;
+import com.tikal.toledo.controllersRest.VO.LoteVO;
 import com.tikal.toledo.dao.LoteDAO;
+import com.tikal.toledo.dao.ProveedorDAO;
 import com.tikal.toledo.model.Lote;
+import com.tikal.toledo.model.Proveedor;
 import com.tikal.toledo.util.JsonConvertidor;
 
 @Controller
@@ -24,6 +28,10 @@ public class LoteController {
 
 	@Autowired
 	LoteDAO lotedao;
+	
+	@Autowired
+	ProveedorDAO pdao;
+	
 	
 	@RequestMapping(value = {
 	"/add" }, method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
@@ -45,7 +53,14 @@ public class LoteController {
 	"/find/{id}" }, method = RequestMethod.GET, produces = "application/json", consumes = "application/json")
 	public void buscar(HttpServletRequest re, HttpServletResponse rs, @PathVariable String id) throws IOException{
 		List<Lote> lotes= lotedao.porProducto(Long.parseLong(id));
-		rs.getWriter().println(JsonConvertidor.toJson(lotes));
+		List<LoteVO> lvos= new ArrayList<LoteVO>();
+		
+		for(Lote l:lotes){
+			LoteVO lvo= new LoteVO(l);
+			Proveedor p= pdao.cargar(l.getProveedor());
+			lvo.setProveedor(p.getNombre());
+		}
+		rs.getWriter().println(JsonConvertidor.toJson(lvos));
 	}
 	
 }
